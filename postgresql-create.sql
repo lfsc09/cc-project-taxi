356002d565ebcb8641f58ed020109647b1ef2913
lfsc09 <lfsc09@gmail.com>
2025-01-21 10:27:11 -0300

    udpate: tried to add inserts, but still not working

--- postgresql-create.sql
+++ postgresql-create.sql
@@ -37,3 +37,59 @@ create table
         long numeric,
         date timestamp
     );
+
+-- insert into
+--     ccca.account (
+--         account_id,
+--         name,
+--         email,
+--         cpf,
+--         car_plate,
+--         is_passenger,
+--         is_driver,
+--         password
+--     )
+-- values
+--     (
+--         '87549960-5557-443e-86c7-3021b3cf9e04'::UUID,
+--         "Jonas Prosta",
+--         "jonas@gmail.com",
+--         "97456321558",
+--         NULL,
+--         'TRUE',
+--         'FALSE',
+--         "123456",
+--     ),
+--     (
+--         '61fa0396-d73a-487c-abf9-83a64bfc7dd4'::UUID,
+--         "Jonas Kaban",
+--         "jkab@gmail.com",
+--         "97456321558",
+--         "ZZZ0000",
+--         'FALSE',
+--         'TRUE',
+--         "123456",
+--     );
+
+-- insert into
+--     ride (
+--         ride_id,
+--         passenger_id,
+--         status,
+--         from_lat,
+--         from_long,
+--         to_lat,
+--         to_long,
+--         date
+--     )
+-- values
+--     (
+--         '0d63a8cf-1607-4b53-b9a4-1d9623caa8f9',
+--         '87549960-5557-443e-86c7-3021b3cf9e04',
+--         'requested',
+--         '-25.4451499',
+--         '-49.307889',
+--         '-25.460001020387935',
+--         '-49.265266248935745',
+--         current_timestamp
+--     );
