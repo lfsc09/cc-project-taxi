257281dd8a7b65de3a649bab0500e535a93b2b36
lfsc09 <lfsc09@gmail.com>
2025-01-21 10:27:52 -0300

    update: Added support for .env

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,23 @@
 import AccountDAODatabase from './infra/dao/Account/AccountDAODatabase';
 import RideDAODatabase from './infra/dao/Ride/RideDAODatabase';
-import PgPromiseAdapter from './infra/database/PostgreSQL';
+import PgPromiseAdapter from './infra/database/PostgresqlAdapter';
 import Registry from './infra/DI';
-import ExpressAdapter from './infra/http/Express';
-
-const POSTGRES_PASS = '123456';
-const POSTGRES_HOST = '127.0.0.1';
-const POSTGRES_PORT = 5432;
-const POSTGRES_DB = 'app';
+import ExpressAdapter from './infra/http/ExpressAdapter';
+import AccountRepositoryDatabase from './infra/repository/Account/AccountRepositoryDatabase';
+import RideRepositoryDatabase from './infra/repository/Ride/RideRepositoryDatabase';
+import AccountController from './infra/routes/AccountController';
+import RideController from './infra/routes/RideController';
 
 try {
     const httpServer = new ExpressAdapter();
     Registry.getInstance().provide('httpServer', httpServer);
-    Registry.getInstance().provide('postgreSQL', new PgPromiseAdapter(POSTGRES_HOST, POSTGRES_PORT, POSTGRES_PASS, POSTGRES_DB));
+    Registry.getInstance().provide('postgreSQL', new PgPromiseAdapter(process.env.POSTGRES_HOST, process.env.POSTGRES_PORT, process.env.POSTGRES_PASS, process.env.POSTGRES_DB));
     Registry.getInstance().provide('accountDAO', new AccountDAODatabase());
+    Registry.getInstance().provide('accountRepository', new AccountRepositoryDatabase());
     Registry.getInstance().provide('rideDAO', new RideDAODatabase());
+    Registry.getInstance().provide('rideRepository', new RideRepositoryDatabase());
+    new AccountController();
+    new RideController();
     httpServer.listen(3000);
 } catch (error: any) {
     console.error(error.message);
